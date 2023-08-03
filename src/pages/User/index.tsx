c96d2ba51dde525b53347c357ae19fca0479ef94
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-03 00:18:46 -0300

    style - Ajuste na pagina de login

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -7,78 +7,66 @@ export function User() {
   return (
     <section className="flex flex-col p-6 pt-24 md:pt-16 md:p-10">
       <p className="text-3xl font-bold text-white w-full">Profile</p>
-      <div className="w-full flex flex-col gap-2 pt-10 ">
-        <div className="w-full flex gap-2 items-center justify-center md:justify-start">
-          <label htmlFor="" className="text-white font-bold w-28">
-            First Name:
-          </label>
-          <input
-            type="text"
-            value={'Silas'}
-            disabled={!changePassword}
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
+      <div className="w-full flex flex-col p-10 gap-6 md:flex-row md:gap-20">
+        <div className="flex flex-col gap-6 items-center">
+          <img
+            src="/src/assets/film.jpg"
+            alt=""
+            className="w-40 h-40 object-cover rounded-full"
           />
-        </div>
-        <div className="w-full flex gap-2 items-center ">
-          <label htmlFor="" className="text-white font-bold w-28">
-            Full name:
-          </label>
-          <input
-            type="text"
-            value={'Silas Oliveira Silva'}
-            disabled={!changePassword}
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
-          />
-        </div>
-        <div className="w-full flex gap-2 items-center ">
-          <label htmlFor="" className="text-white font-bold w-28">
-            E-mail:
-          </label>
-          <input
-            type="text"
-            value={'silas-oli@hotmail.com'}
-            disabled={!changePassword}
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
-          />
+          <p className="text-lg font-bold text-white">Silas SIlva</p>
         </div>
 
-        <>
-          <div className="w-full flex gap-2 items-center">
-            <label htmlFor="" className="text-white font-bold w-28">
-              Password:
-            </label>
-            <input
-              type="text"
-              value={'Silas Oliveira Silva'}
-              disabled={!changePassword}
-              className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
-            />
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="text-2xl font-bold text-white">Profile</p>
+          <div className="flex flex-col gap-2 pt-10 ">
+            <div className="w-full flex gap-2 items-center justify-center md:justify-start">
+              <label htmlFor="" className="text-white font-bold w-28">
+                First Name:
+              </label>
+              <input
+                type="text"
+                value={'Silas'}
+                disabled={!changePassword}
+                className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
+              />
+            </div>
+            <div className=" flex gap-2 items-center ">
+              <label htmlFor="" className="text-white font-bold w-28">
+                Full name:
+              </label>
+              <input
+                type="text"
+                value={'Silas Oliveira Silva'}
+                disabled={!changePassword}
+                className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
+              />
+            </div>
+            <div className=" flex gap-2 items-center ">
+              <label htmlFor="" className="text-white font-bold w-28">
+                E-mail:
+              </label>
+              <input
+                type="text"
+                value={'silas-oli@hotmail.com'}
+                disabled={!changePassword}
+                className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
+              />
+            </div>
+            <div className=" flex justify-center p-6 md:justify-end md:p-t-6 md:px-0">
+              {changePassword ? (
+                <Button
+                  text="Cancel edit"
+                  onClick={() => setChangePassword((state) => !state)}
+                />
+              ) : (
+                <Button
+                  text="Edit Profile"
+                  onClick={() => setChangePassword((state) => !state)}
+                />
+              )}
+            </div>
           </div>
-          <div className="w-full flex gap-2 items-center">
-            <label htmlFor="" className="text-white font-bold w-28">
-              Confirm password:
-            </label>
-            <input
-              type="text"
-              value={'Silas Oliveira Silva'}
-              disabled={!changePassword}
-              className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white max-w-lg"
-            />
-          </div>
-        </>
-
-        <div className="w-full flex justify-center p-6 md:justify-start md:p-t-6 md:px-0">
-          {changePassword ? (
-            <Button
-              text="Cancel edit"
-              onClick={() => setChangePassword((state) => !state)}
-            />
-          ) : (
-            <Button
-              text="Edit Profile"
-              onClick={() => setChangePassword((state) => !state)}
-            />
-          )}
         </div>
       </div>
     </section>
