947fbd9ffb58a21b5a0f0a64c8df5a0ece58c48e
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-04 01:10:17 -0300

    style - Ajuste de estilização

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -8,24 +8,22 @@ export function User() {
     <section className="flex flex-col p-6 pt-28 gap-10 md:pt-16 md:p-10">
       <p className="text-3xl font-bold text-white">Profile</p>
 
-      <div className="flex w-full flex-col gap-10 md:flex-row bg-purple">
-        <div className="flex flex-col gap-6 p-6 items-center">
+      <div className="flex w-full flex-col items-center p-6 gap-6 bg-red-400 rounded-lg md:items-start md:max-w-[70vw] md:flex-row">
+        <div className="flex flex-col items-center gap-4">
           <img
             src="/src/assets/film.jpg"
             alt=""
             className="w-40 h-40 object-cover rounded-full"
           />
-          <p className="text-lg font-bold text-white">Silas SIlva</p>
+          <p className="text-lg font-bold text-white">Silas Silva</p>
         </div>
-
-        <div className="flex flex-1  flex-col items-center gap-4 p-6 md:items-start bg-slate-400">
-          <p className="text-2xl font-bold text-white">Profile</p>
-          <div className="w-full flex">
-            <div className="w-full flex">
-              <label htmlFor="">Name:</label>
-              <input type="text" className="flex-1" />
-            </div>
-          </div>
+        <div className="flex flex-col w-full gap-6">
+          <label className="text-white font-bold">Name:</label>
+          <input
+            type="text"
+            className="w-max-[40vw] bg-transparent border-2 border-red-500 rounded-lg p-2 text-white"
+            value={'Silas'}
+          />
         </div>
       </div>
     </section>
