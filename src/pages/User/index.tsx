b75a1b494972657465b4274f379dbfd645288a8a
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-05 00:32:43 -0300

    style - Ajuste na estilização

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -8,22 +8,46 @@ export function User() {
     <section className="flex flex-col p-6 pt-28 gap-10 md:pt-16 md:p-10">
       <p className="text-3xl font-bold text-white">Profile</p>
 
-      <div className="flex w-full flex-col items-center p-6 gap-6 bg-red-400 rounded-lg md:items-start md:max-w-[70vw] md:flex-row">
+      <div className="flex w-full flex-wrap justify-center p-6 gap-16 bg-light-gray rounded-lg md:items-start md:max-w-[60vw] md:flex-row">
         <div className="flex flex-col items-center gap-4">
           <img
             src="/src/assets/film.jpg"
             alt=""
             className="w-40 h-40 object-cover rounded-full"
           />
-          <p className="text-lg font-bold text-white">Silas Silva</p>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <p className="text-lg font-bold text-white">Silas Silva</p>
+            <p className="font-bold text-red-500">Web developer</p>
+          </div>
         </div>
-        <div className="flex flex-col w-full gap-6">
+        <div className="flex flex-1 flex-col gap-2 ">
           <label className="text-white font-bold">Name:</label>
           <input
             type="text"
-            className="w-max-[40vw] bg-transparent border-2 border-red-500 rounded-lg p-2 text-white"
+            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray"
             value={'Silas'}
           />
+          <label className="text-white font-bold">E-mail:</label>
+          <input
+            type="text"
+            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
+            value={'silas-oli@hotmail.com'}
+          />
+          <label className="text-white font-bold">Name:</label>
+          <input
+            type="text"
+            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
+            value={'Silas'}
+          />
+          <label className="text-white font-bold">Name:</label>
+          <input
+            type="text"
+            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
+            value={'Silas'}
+          />
+          <div className="flex items-center pt-6 justify-center md:justify-end">
+            <Button text="Edit profile" />
+          </div>
         </div>
       </div>
     </section>
