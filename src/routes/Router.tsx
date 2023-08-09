b1af749156603f8ee62cc29247b9de6708c9d833
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-08 23:47:13 -0300

    feature - novas rotas

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,6 +6,9 @@ import { Favourites } from '../pages/Favourites';
 import { Trending } from '../pages/Trending';
 import { User } from '../pages/User';
 import { Community } from '../pages/Community';
+import { Register } from '../pages/Register';
+import { Login } from '../pages/Login';
+import { AccountLoginAndRegister } from '../layouts/AccountLoginAndRegister';
 
 export function Router() {
   return (
@@ -16,6 +19,11 @@ export function Router() {
         <Route path="/trending" element={<Trending />} />
         <Route path="/user" element={<User />} />
         <Route path="/community" element={<Community />} />
+      </Route>
+
+      <Route path="/account" element={<AccountLoginAndRegister />}>
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
