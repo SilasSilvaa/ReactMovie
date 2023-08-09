08985aef7225edc9ba504e2c3769f48112a9eebe
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-08 23:49:10 -0300

    refactor - Substituição de trechos de códigos por componentes

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from '../../components/Button/Button';
+import { Input } from '../../components/Input/Input';
 
 export function User() {
   const [changePassword, setChangePassword] = useState(false);
@@ -8,12 +9,12 @@ export function User() {
     <section className="flex flex-col p-6 pt-28 gap-10 md:pt-16 md:p-10">
       <p className="text-3xl font-bold text-white">Profile</p>
 
-      <div className="flex w-full flex-wrap justify-center p-6 gap-6 bg-light-gray rounded-lg md:items-start md:max-w-[60vw] md:flex-row">
+      <div className="flex w-full flex-wrap justify-center p-6 gap-6 bg-black rounded-lg border-2 border-red-500 md:items-start md:max-w-[60vw] md:flex-row">
         <div className="flex flex-col items-center gap-4">
           <img
             src="/src/assets/film.jpg"
             alt=""
-            className="w-40 h-40 object-cover rounded-full"
+            className="w-40 h-40 object-cover rounded-full border-2 border-red-500"
           />
           <div className="flex flex-col items-center justify-center gap-2">
             <p className="text-lg font-bold text-white">Silas Silva</p>
@@ -22,29 +23,17 @@ export function User() {
         </div>
         <div className="flex flex-1 flex-col gap-2 ">
           <label className="text-white font-bold">Name:</label>
-          <input
-            type="text"
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray"
-            value={'Silas'}
-          />
+          <Input type="text" value={'Silas'} />
+
+          <label className="text-white font-bold">Full name:</label>
+          <Input type="text" value={'Teste Teste Teste'} />
+
           <label className="text-white font-bold">E-mail:</label>
-          <input
-            type="text"
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
-            value={'silas-oli@hotmail.com'}
-          />
-          <label className="text-white font-bold">Name:</label>
-          <input
-            type="text"
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
-            value={'Silas'}
-          />
-          <label className="text-white font-bold">Name:</label>
-          <input
-            type="text"
-            className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray "
-            value={'Silas'}
-          />
+          <Input type="text" value={'teste@teste.com'} />
+
+          <label className="text-white font-bold">Job:</label>
+          <Input type="text" value={'Teste Teste'} />
+
           <div className="flex items-center pt-6 justify-center md:justify-end">
             <Button text="Edit profile" />
           </div>
