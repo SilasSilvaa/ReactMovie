aafa16d5f28f8597f51c8a7cd9000e2b13008dc0
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-09 22:24:28 -0300

    refactor - alteração no componente responsavel pelo title da aplicação

--- src/components/MenuComponents/Header/Title.tsx
+++ src/components/MenuComponents/Header/Title.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FilmReel } from '@phosphor-icons/react';
 
-export function Header() {
+export function Title() {
   return (
     <header className="flex items-center">
       <FilmReel className="w-12 h-12 text-red-500" />
