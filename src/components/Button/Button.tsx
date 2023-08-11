7ffd2496514785bc301049e0d3d292056d63476e
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-10 23:33:15 -0300

    feat - adicionando o tailwind merge no componente

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import React, { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-type ButtonProps = ComponentProps<'button'>;
+type ButtonProps = ComponentProps<'button'> & {
+  className?: string;
+};
 
-export function Button({ ...props }: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="border-none bg-red-500 text-white font-bold p-4 rounded-xl hover:bg-red-600 transition-colors ease-in-out"
+      className={twMerge(
+        'border-none bg-red-500 text-white font-bold p-4 rounded-xl hover:bg-red-600 transition-colors ease-in-out',
+        className
+      )}
       {...props}
     />
   );
