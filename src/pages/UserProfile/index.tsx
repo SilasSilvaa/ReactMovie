2551274e4c8493eef4247dcb56a7a9f9cd1fde0e
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-10 23:34:05 -0300

    refactor - aplicando nova estilização no modal de perfil

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -4,36 +4,55 @@ import { Input } from '../../components/Input/Input';
 
 export function UserProfile() {
   return (
-    <div className="flex w-full flex-col pt-20 gap-10 px-6">
-      <p className="text-3xl font-bold text-white">Profile</p>
+    <div className="flex w-full flex-col justify-center items-center gap-6 p-6">
+      <p className="mediumTitle flex justify-center p-2">Profile</p>
 
-      <div className="flex w-full flex-wrap justify-center p-6 gap-6 bg-black rounded-lg border-2 border-red-500 md:items-start md:max-w-[60vw] md:flex-row">
-        <div className="flex flex-col items-center gap-4">
-          <img
-            src="/src/assets/film.jpg"
-            alt=""
-            className="w-40 h-40 object-cover rounded-full border-2 border-red-500"
-          />
-          <div className="flex flex-col items-center justify-center gap-2">
-            <p className="text-lg font-bold text-white">Silas Silva</p>
-            <p className="font-bold text-red-500">Web developer</p>
+      <div className="w-full flex flex-row flex-wrap items-center justify-center max-w-4xl gap-6 bg-red-900 rounded-lg p-2">
+        <div className="w-full flex items-center justify-center bg-slate-500 rounded-lg md:w-80 ">
+          <div className="flex items-center flex-col p-6 gap-6">
+            <img
+              src="/src/assets/film.jpg"
+              className="rounded-full w-24 h-24 object-cover"
+            />
+            <div className="flex flex-col items-center">
+              <span className="textCss text-black">Silas Silva</span>
+              <span className="text-black">Dev FullStack</span>
+            </div>
+
+            <div className="flex flex-wrap gap-2 items-center justify-center">
+              <Button className="p-2 "> Edit photo</Button>
+              <Button className="p-2"> Edit profile</Button>
+            </div>
           </div>
         </div>
-        <div className="flex flex-1 flex-col gap-2 ">
-          <label className="text-white font-bold">Name:</label>
-          <Input type="text" value={'Silas'} />
-
-          <label className="text-white font-bold">Full name:</label>
-          <Input type="text" value={'Teste Teste Teste'} />
-
-          <label className="text-white font-bold">E-mail:</label>
-          <Input type="text" value={'teste@teste.com'} />
-
-          <label className="text-white font-bold">Job:</label>
-          <Input type="text" value={'Teste Teste'} />
-
-          <div className="flex items-center pt-6 justify-center md:justify-end">
-            <Button>Edit profile</Button>
+        {/* <Input /> */}
+        <div className="flex flex-1 flex-col gap-4 p-6">
+          <div className="flex flex-1 gap-2">
+            <label htmlFor="" className="font-bold text-white">
+              Name
+            </label>
+            <Input />
+          </div>
+          <div className="flex flex-1 gap-2">
+            <label htmlFor="" className="font-bold text-white">
+              Full name
+            </label>
+            <Input />
+          </div>
+          <div className="flex flex-1 gap-2">
+            <label htmlFor="" className="font-bold text-white">
+              E-mail
+            </label>
+            <Input />
+          </div>
+          <div className="flex flex-1 gap-2">
+            <label htmlFor="" className="font-bold text-white">
+              password
+            </label>
+            <Input />
+          </div>
+          <div>
+            <span>View password</span>
           </div>
         </div>
       </div>
