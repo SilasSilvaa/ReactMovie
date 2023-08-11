6c1fb39bd1dcac126bff482530d102838eae36e5
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-10 23:34:42 -0300

    feat - adicionando tailwind merge no componente

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,17 @@
 import React, { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-type InputProps = ComponentProps<'input'>;
+type InputProps = ComponentProps<'input'> & {
+  className?: string;
+};
 
-export function Input({ ...props }: InputProps) {
+export function Input({ className, ...props }: InputProps) {
   return (
     <input
-      className="flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray outline-none"
+      className={twMerge(
+        'flex-1 bg-transparent border-2 border-red-500 rounded-lg p-2 text-white placeholder:text-gray outline-none',
+        className
+      )}
       {...props}
     />
   );
