bc442fac6a8ee4bce1e9298eb33647d897de12ae
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-13 23:33:39 -0300

    feat -  renderizando dados recebidos via props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,18 +2,33 @@ import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import { Button } from '../Button/Button';
 import { Heart } from '@phosphor-icons/react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 interface CardProps {
+  detail: {
+    id: string;
+    name: string;
+    date: string;
+    type: string;
+    imageMovie: string;
+  };
   className?: string;
 }
 
-export function Card({ className }: CardProps) {
+export function Card({ className, detail }: CardProps) {
+  const navigate = useNavigate();
+
+  function viewDetail(id: string) {
+    navigate(`details/${id}`);
+  }
+
   return (
     <div
       className={twMerge(
-        'flex relative flex-col w-80 h-80 rounded-lg',
+        'flex relative flex-col w-80 h-80 rounded-lg cursor-pointer',
         className
       )}
+      onClick={() => viewDetail(detail.id)}
     >
       <div className="absolute right-4 top-4">
         <Button className="p-2">
@@ -21,13 +36,15 @@ export function Card({ className }: CardProps) {
         </Button>
       </div>
       <img
-        src="/src/assets/film.jpg"
+        src={detail.imageMovie}
         alt=""
         className="w-full h-full object-cover rounded-lg"
       />
       <div className="flex absolute bottom-0 bg-opacity-95 flex-col items-start text-center w-full p-4 bg-white rounded-b-lg ">
-        <span className="font-bold">New Film</span>
-        <span>2022 | Action comedy</span>
+        <span className="font-bold">{detail.name}</span>
+        <span>
+          {detail.date} | {detail.type}{' '}
+        </span>
       </div>
     </div>
   );
