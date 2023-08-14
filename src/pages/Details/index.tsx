2fd03538b5dc0df1f987d05f8cbc6486f3f4bf0c
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-13 23:34:07 -0300

    feat - nova pagina responsavel pelos detalhes

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Banner } from '../../components/Banner/Banner';
+
+export function Details() {
+  const { id } = useParams();
+
+  return <></>;
+}
