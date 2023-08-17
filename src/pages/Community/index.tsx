d3198fbab150a78e95b2dff1e8830a28a137a9d7
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-16 23:35:03 -0300

    refactor - Atualizando botões para components

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -6,6 +6,7 @@ import {
   X,
 } from '@phosphor-icons/react';
 import React, { useState } from 'react';
+import { Button } from '../../components/Button/Button';
 export function Community() {
   const [newPublication, setNewPublication] = useState(false);
   const [newComment, setNewComment] = useState(false);
@@ -61,17 +62,18 @@ export function Community() {
               </span>
             </div>
             <div className="flex w-full gap-4 p-2">
-              <button>
+              <Button className="bg-transparent hover:bg-transparent p-0">
                 <ThumbsUp className="text-white w-7 h-7" />
-              </button>
-              <button>
+              </Button>
+              <Button className="bg-transparent hover:bg-transparent p-0">
                 <ChatCircle
                   className="text-white w-7 h-7"
                   onClick={handleAddComment}
                 />
-              </button>
+              </Button>
             </div>
             <hr className="border border-white" />
+            {/* Comments */}
             <span className="text-white font-bold">Comments</span>
             <div className="flex flex-col p-2 gap-6">
               <div className="flex flex-col gap-2 items-start md:flex-row md:items-center ">
@@ -108,52 +110,12 @@ export function Community() {
                     type="text"
                     className="flex flex-1 p-1 rounded-lg border-2 border-red-500 bg-white text-black"
                   />
-                  <button className="rounded-lg bg-red-500 flex items-center justify-center p-2">
+                  <Button className="p-2">
                     <PaperPlaneRight className="text-white" />
-                  </button>
+                  </Button>
                 </div>
               </div>
             )}
-          </div>
-          {/*  */}
-          <div className="flex w-full max-w-4xl flex-col bg-black border-2 border-red-500 rounded-lg gap-6 p-6">
-            <div className="flex justify-between">
-              <div className="w-10 h-10 bg-purple rounded-full"></div>
-              <span className="text-white">Há 1 hora</span>
-            </div>{' '}
-            <div className="flex flex-col gap-4">
-              <div>
-                <span className="text-white p-2 rounded-lg bg-red-500">
-                  topic
-                </span>
-              </div>
-              <span className="text-white flex">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-                itaque exercitationem.
-              </span>
-            </div>
-            <div className="flex w-full p-2 gap-4">
-              <button>
-                <ThumbsUp className="text-white w-7 h-7" />
-              </button>
-              <button>
-                <ChatCircle className="text-white w-7 h-7" />
-              </button>
-            </div>
-            <hr className="border border-white" />
-            <span className="text-white font-bold">Comments</span>
-            <div className="flex flex-col p-2 gap-6">
-              <div className="flex flex-col gap-2 items-start md:flex-row md:items-center ">
-                <div className="w-8 h-8 rounded-full bg-blue-500"></div>
-                <div>
-                  <span className="text-white">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Modi, minima? Est accusamus autem rem
-                  </span>
-                </div>
-              </div>
-              {/*  */}
-            </div>
           </div>
         </div>
       </div>
