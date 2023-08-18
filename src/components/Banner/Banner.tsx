db1c81468435c4e3bb201d0ecb04ded4748c5b21
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-17 23:51:04 -0300

    feat - criado a função para atualizar o banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Filter } from '../Filter/Filter';
 import { Button } from '../Button/Button';
 import { Heart } from '@phosphor-icons/react';
-import { MoviesProps } from '../../context/MovieContext';
+import { MovieContext, MoviesProps } from '../../context/MovieContext';
 
 interface dataProps {
   data: MoviesProps;
 }
 
 export function Banner({ data }: dataProps) {
+  const { movies } = useContext(MovieContext);
+  const filterArrayMovies = movies.slice(0, 3);
+  const [filteredMovies, setFilteredMovies] = useState<MoviesProps[]>([]);
+
+  useEffect(() => {
+    if (filterArrayMovies) {
+      setFilteredMovies(filterArrayMovies);
+    }
+  }, []);
+
+  console.log(filterArrayMovies);
+
+  const [index, setIndex] = useState(0);
+
+  function handleChangeBanner(i: number) {
+    setIndex(i);
+  }
+
   return (
     <div className="flex w-full relative items-center flex-col justify-center lg:h-auto">
       <Filter />
       <img
-        src={`https://image.tmdb.org/t/p/original/${data?.backdrop_path}`}
+        src={
+          movies &&
+          `https://image.tmdb.org/t/p/original${filteredMovies[index]?.backdrop_path}`
+        }
         alt=""
         className=" object-cover w-full min-h-[55vh] max-h-[65vh]"
       />
 
       <div className="absolute left-10 bottom-10 flex flex-col">
-        <h3 className="text-white font-bold text-3xl">{data?.title}</h3>
+        <h3 className="text-white font-bold text-3xl">
+          {movies !== undefined && filteredMovies[index]?.title}
+        </h3>
         <span className="text-light-gray">2022 | Comedy horror | 1h30 </span>
         <div className="flex pt-10 gap-4">
           <Button>Watch trailer now</Button>
@@ -29,9 +52,18 @@ export function Banner({ data }: dataProps) {
         </div>
       </div>
       <div className="flex gap-2 absolute bottom-2">
-        <div className="w-2 h-2 bg-white rounded-full cursor-pointer"></div>
-        <div className="w-2 h-2 bg-gray rounded-full cursor-pointer"></div>
-        <div className="w-2 h-2 bg-gray rounded-full cursor-pointer"></div>
+        <div
+          className="w-2 h-2 bg-white rounded-full cursor-pointer"
+          onClick={() => handleChangeBanner(0)}
+        ></div>
+        <div
+          className="w-2 h-2 bg-gray rounded-full cursor-pointer"
+          onClick={() => handleChangeBanner(1)}
+        ></div>
+        <div
+          className="w-2 h-2 bg-gray rounded-full cursor-pointer"
+          onClick={() => handleChangeBanner(2)}
+        ></div>
       </div>
     </div>
   );
