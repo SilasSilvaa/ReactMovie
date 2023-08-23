b3068a45988b79c7ea3c2e9b8c2bd08494ea7fa5
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-22 22:37:37 -0300

    feat - utilizando a biblioteca daisyui para o carrossel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,32 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Banner } from '../../components/Banner/Banner';
 import { Card } from '../../components/Card/Card';
 import { Button } from '../../components/Button/Button';
 import { MovieContext } from '../../context/MovieContext';
 
 export function Home() {
-  const { movies } = useContext(MovieContext);
+  const { nowPlayingMovies, getMoviesNowPlaying } = useContext(MovieContext);
 
+  useEffect(() => {
+    getMoviesNowPlaying();
+  }, []);
+
+  console.log(nowPlayingMovies);
   return (
     <>
       <section className="flex flex-col gap-4 pt-20 lg:pt-0">
         {/* <ButtonToggleTheme /> */}
-        <Banner data={movies[5]} />
+        <Banner />
 
         <div className="containerCard">
           <p className="mediumTitle">Trending</p>
           <div className="contentCard">
-            {/* {movies.map((movie) => (
-              <Card detail={movie} key={movie.id} />
-            ))} */}
-            <div className="w-full flex justify-center p-2 lg:justify-end">
+            <div className="carousel max-w-full gap-6 rounded-box">
+              {nowPlayingMovies.map((movie) => (
+                <Card detail={movie} key={movie.id} className="carousel-item" />
+              ))}
+            </div>
+            <div className="w-full flex justify-end p-2">
               <Button>View All</Button>
             </div>
           </div>
@@ -27,10 +34,12 @@ export function Home() {
         <div className="containerCard">
           <p className="mediumTitle">In High</p>
           <div className="contentCard">
-            {/* {cards.map((card) => (
-              // <Card detail={card} key={card.id} />
-            ))} */}
-            <div className="w-full flex justify-center p-2 lg:justify-end">
+            <div className="carousel max-w-full gap-6 rounded-box">
+              {nowPlayingMovies.map((movie) => (
+                <Card detail={movie} key={movie.id} className="carousel-item" />
+              ))}
+            </div>
+            <div className="w-full flex justify-end p-2">
               <Button>View All</Button>
             </div>
           </div>
@@ -39,10 +48,12 @@ export function Home() {
         <div className="  containerCard">
           <p className="mediumTitle">Top films</p>
           <div className="contentCard">
-            {/* {cards.map((card) => (
-              // <Card detail={card} key={card.id} />
-            ))} */}
-            <div className="w-full flex justify-center p-2 lg:justify-end">
+            <div className="carousel max-w-full gap-6 rounded-box">
+              {nowPlayingMovies.map((movie) => (
+                <Card detail={movie} key={movie.id} className="carousel-item" />
+              ))}
+            </div>
+            <div className="w-full flex justify-end p-2">
               <Button>View All</Button>
             </div>
           </div>
