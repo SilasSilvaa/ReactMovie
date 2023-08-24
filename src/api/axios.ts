f15be0d63b51e7b8e7287a6206227ec1f4fad613
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-24 00:03:52 -0300

    feat - parametro adicionado

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,9 @@ export const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjBhOWFmZGM2YjFjZDNiZWY1OGY1OWNmNmZkYmJjZiIsInN1YiI6IjY0ZGMwZGRhZjQ5NWVlMDI5MjUxMTVkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ukNdix_J_GcuBkNqiwzdFmsDMMoKo4Uff7O79Ls9Pwc'
+    },
+    params: {
+        language: 'pt-BR',
     }
 })
 
