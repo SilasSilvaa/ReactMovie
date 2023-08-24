273501800b5aedfb218b0da50bd634451102f3fd
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-24 00:06:44 -0300

    feat - pagina de detalhes com dados dinamicos

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Button } from '../../components/Button/Button';
 import { Heart } from '@phosphor-icons/react';
 import { Card } from '../../components/Card/Card';
+import { MovieContext } from '../../context/MovieContext';
 
 export function Details() {
+  const { id } = useParams();
+  const { detailMovie, getDetails } = useContext(MovieContext);
+
+  useEffect(() => {
+    if (id) {
+      getDetails(id);
+    }
+  }, []);
+
+  console.log(detailMovie);
+
   return (
     <>
       <section className="contentCard pt-20 md:pt-0 md:p-12">
@@ -12,7 +24,7 @@ export function Details() {
           <div className="flex-1">
             <div className="flex relative flex-col w-full rounded-lg">
               <img
-                src="/src/assets/film.jpg"
+                src={`https://image.tmdb.org/t/p/original/${detailMovie?.backdrop_path}`}
                 alt=""
                 className="h-96 object-cover rounded-lg cursor-pointer"
               />
@@ -22,7 +34,7 @@ export function Details() {
           <div className="flex flex-col justify-center flex-1 gap-6">
             <div className="w-full flex justify-between flex-wrap gap-6">
               <div>
-                <h3 className="mediumTitle">New movie</h3>
+                <h3 className="mediumTitle">{detailMovie?.original_title}</h3>
                 <span className="text-white">2022 | Action comedy</span>
               </div>
               <div className="flex gap-4">
@@ -34,28 +46,10 @@ export function Details() {
               </div>
             </div>
             <div className="flex flex-col pt-10 gap-6">
-              <span className="mediumTitle">About this movie</span>
+              <span className="mediumTitle">Overview</span>
 
               <p className="text-white leading-6 max-w-4xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-                et porro ex placeat commodi earum accusamus, recusandae
-                dignissimos enim consequatur doloremque maiores deleniti
-                repudiandae impedit ea quaerat expedita fugit. Magni. Labore
-                magnam dignissimos facilis harum molestias consequatur animi.
-                Non corporis ipsam nulla repellat vero aperiam illo tempora
-                voluptatem? Ullam ad, omnis ipsum sint consequatur earum tempora
-                voluptas! Repudiandae, iure nam. Aliquam sequi quisquam eius
-                dicta, odit dolorum recusandae quibusdam ad similique officiis
-                iusto reprehenderit corrupti, ratione molestiae saepe. Suscipit
-                illo quos doloribus alias porro quod hic pariatur, cupiditate
-                odio commodi! Doloribus, facilis iste? Quae, maiores fugit eius
-                corporis repellendus ducimus, enim, hic alias provident esse
-                soluta reiciendis inventore porro ad at officia nisi vel iste
-                accusantium repellat dolor eum. Impedit? Totam hic excepturi
-                voluptate omnis officia eos, a inventore veniam perferendis ea
-                perspiciatis itaque eum autem nostrum, fugit accusantium nam
-                voluptates dolore alias? In vero saepe autem, iure fuga
-                repellat.
+                {detailMovie?.overview}
               </p>
             </div>
           </div>
