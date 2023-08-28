c00cfded4e19908b9676f1b2f2c513cd95c4e0de
SilasSilvaa <silas-oli@hotamil.com>
2023-08-28 12:13:15 -0300

    fix - bug para redicionar para detalhes ajustado

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
 
 import { Button } from '../Button/Button';
@@ -12,32 +12,37 @@ interface CardProps {
 }
 
 export function Card({ className, detail }: CardProps) {
+  const navigate = useNavigate();
+
+  function showMovieDetails() {
+    navigate(`/details/${detail.id}`);
+  }
+
   return (
-    <Link to={`/details/${detail.id}`}>
-      <div
-        className={twMerge(
-          'flex relative flex-col w-80 h-80 rounded-lg',
-          className
-        )}
-      >
-        <div className="absolute right-4 top-4">
-          <Button className="p-2">
-            <Heart size={25} className="fill-white " />
-          </Button>
-        </div>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
-          alt=""
-          className="w-full h-full object-cover rounded-lg cursor-pointer"
-        />
-        <div
-          className="flex absolute bottom-0 bg-opacity-95 flex-col items-start text-center w-full 
-      p-4 bg-white rounded-b-lg "
-        >
-          <span className="font-bold text-black">{detail.title}</span>
-          {/* <span>{detail.release_date}</span> */}
-        </div>
+    <div
+      className={twMerge(
+        'flex relative flex-col w-80 h-80 rounded-lg',
+        className
+      )}
+      onClick={showMovieDetails}
+    >
+      <div className="absolute right-4 top-4">
+        <Button className="p-2">
+          <Heart size={25} className="fill-white " />
+        </Button>
       </div>
-    </Link>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+        alt=""
+        className="w-full h-full object-cover rounded-lg cursor-pointer"
+      />
+      <div
+        className="flex absolute bottom-0 bg-opacity-95 flex-col items-start text-center w-full 
+      p-4 bg-white rounded-b-lg "
+      >
+        <span className="font-bold text-black">{detail.title}</span>
+        {/* <span>{detail.release_date}</span> */}
+      </div>
+    </div>
   );
 }
