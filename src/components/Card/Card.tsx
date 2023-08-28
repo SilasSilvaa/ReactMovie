5463e9f487d435a56aafca72c761eb39845ad58e
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-27 23:09:41 -0300

    refactor - ajuste no link para vero detalhes do filme

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,31 +13,31 @@ interface CardProps {
 
 export function Card({ className, detail }: CardProps) {
   return (
-    // <Link to={`details/${detail.id}`}>
-    <div
-      className={twMerge(
-        'flex relative flex-col w-80 h-80 rounded-lg',
-        className
-      )}
-    >
-      <div className="absolute right-4 top-4">
-        <Button className="p-2">
-          <Heart size={25} className="fill-white " />
-        </Button>
-      </div>
-      <img
-        src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
-        alt=""
-        className="w-full h-full object-cover rounded-lg cursor-pointer"
-      />
+    <Link to={`/details/${detail.id}`}>
       <div
-        className="flex absolute bottom-0 bg-opacity-95 flex-col items-start text-center w-full 
-      p-4 bg-white rounded-b-lg "
+        className={twMerge(
+          'flex relative flex-col w-80 h-80 rounded-lg',
+          className
+        )}
       >
-        <span className="font-bold text-black">{detail.title}</span>
-        {/* <span>{detail.release_date}</span> */}
+        <div className="absolute right-4 top-4">
+          <Button className="p-2">
+            <Heart size={25} className="fill-white " />
+          </Button>
+        </div>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+          alt=""
+          className="w-full h-full object-cover rounded-lg cursor-pointer"
+        />
+        <div
+          className="flex absolute bottom-0 bg-opacity-95 flex-col items-start text-center w-full 
+      p-4 bg-white rounded-b-lg "
+        >
+          <span className="font-bold text-black">{detail.title}</span>
+          {/* <span>{detail.release_date}</span> */}
+        </div>
       </div>
-    </div>
-    // {/* </Link> */}
+    </Link>
   );
 }
