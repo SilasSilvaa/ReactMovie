2f40f9302163d6bbbb0a62bf9ec455354ad0f8f1
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-28 23:20:19 -0300

    feat - adicionado QueryClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './routes/Router';
 import { MovieContextProvider } from './context/MovieContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <MovieContextProvider>
-        <Router />
-      </MovieContextProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MovieContextProvider>
+          <Router />
+        </MovieContextProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
