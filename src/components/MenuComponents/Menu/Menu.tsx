4e848036873a122706bf43ceecda0739f1b6f4ee
SilasSilvaa <silas-oli@hotamil.com>
2023-08-29 16:26:57 -0300

    refacto - ajuste no css

--- src/components/MenuComponents/Menu/Menu.tsx
+++ src/components/MenuComponents/Menu/Menu.tsx
@@ -5,7 +5,7 @@ import { Title } from '../Header/Title';
 export function Menu() {
   return (
     <>
-      <aside className="hidden h-screen flex-col bg-black p-6 lg:flex ">
+      <aside className="hidden h-screen flex-col bg-black py-4 px-10 lg:flex ">
         <Title />
         <MenuOptions />
       </aside>
