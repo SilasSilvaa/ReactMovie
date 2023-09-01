f88a0b9ef858ec8ce7e040c7e5e2ef4087511b59
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-31 22:45:52 -0300

    feat - pagina de trending consumindos dados da api

--- src/pages/Trending/index.tsx
+++ src/pages/Trending/index.tsx
@@ -1,38 +1,52 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
 import { Filter } from '../../components/Filter/Filter';
 // import { Card } from '../../components/Card/Card';
 import { Button } from '../../components/Button/Button';
 import { Heart } from '@phosphor-icons/react';
+import { useGetAllMovies } from '../../hooks/useGetAllMovies';
+import { Card } from '../../components/Card/Card';
 
 export function Trending() {
+  const { data } = useQuery('trendingMovies', () =>
+    useGetAllMovies('movie/popular')
+  );
+
   return (
     <section className="flex flex-col pt-20 lg:pt-0">
       <div className="relative pb-10">
         <Filter />
       </div>
 
-      <div className="flex  flex-col gap-4 p-4 pt-20 items-center md:items-start">
+      <div className="flex  flex-col gap-4 p-4 pt-20 justify-center items-center md:items-start">
         <h3 className="mediumTitle">Trending</h3>
 
-        <div className="flex flex-col w-full justify-between gap-6 flex-wrap p-6 md:flex md:flex-row ">
-          <div className="flex-1">
-            {/* <Card className="w-full h-full" /> */}
+        <div className="flex flex-col w-full gap-6 flex-wrap md:flex md:flex-row ">
+          <div className="max-w-4xl">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${
+                data && data[0]?.backdrop_path
+              }`}
+              alt=""
+              className="rounded-lg"
+            />
           </div>
 
-          <div className="flex flex-col justify-center flex-1">
-            <h3 className="mediumTitle">New movie</h3>
+          <div className="flex flex-col justify-center flex-1 gap-4">
+            <h3 className="mediumTitle">{data && data[0]?.title}</h3>
             <span className="text-white">2022 | Action comedy</span>
-            <p className="text-white text-start py-6">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-              obcaecati ullam molestias in eligendi fugiat laborum, assumenda
-              unde! Omnis, modi perferendis voluptatem totam illum aperiam
-              excepturi tenetur fuga sapiente enim. Obcaecati ullam molestias in
-              eligendi fugiat laborum, assumenda unde! Omnis, modi perferendis
-              voluptatem totam illum aperiam excepturi tenetur fuga sapiente
-              enim.
-            </p>
+            {/* <p className="text-white text-start py-6 flex-1">
+              {data && data[0]?.}
+            </p> */}
             <div className="flex gap-4">
-              <Button>Watch Trailer</Button>
+              <Link
+                to={`https://www.youtube.com/results?search_query=${
+                  data && data[0]?.title + 'Trailer'
+                }`}
+              >
+                <Button>Watch Trailer</Button>
+              </Link>
 
               <Button>
                 <Heart size={25} className="fill-white " />
@@ -43,10 +57,14 @@ export function Trending() {
         <div className="flex w-full py-4 px-6 justify-start md:items-start">
           <h3 className="mediumTitle">Recently released</h3>
         </div>
-        <div className="contentCard">
-          {/* <Card />
-          <Card />
-          <Card /> */}
+        <div className=" carousel max-w-full gap-6 rounded-box">
+          {data?.map((trendindMovie) => (
+            <Card
+              detail={trendindMovie}
+              key={trendindMovie.id}
+              className="carousel-item"
+            />
+          ))}
         </div>
       </div>
     </section>
