569f6ef1dfc64a1f08cf50e95bc121ad32da64a2
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-31 23:57:56 -0300

    refactor - utilizando a sidebar junto com o menu

--- src/components/MenuComponents/Menu/Menu.tsx
+++ src/components/MenuComponents/Menu/Menu.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MenuOptions } from '../MenuOptions/MenuOptions';
-import { Title } from '../Header/Title';
+import { Header } from '../Header/Header';
+import { SideBar } from '../SideBar/SideBar';
 
 export function Menu() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <>
-      <aside className="hidden h-screen flex-col bg-black py-4 px-10 lg:flex ">
-        <Title />
-        <MenuOptions />
+      <aside className="hidden flex-1 flex-col bg-black lg:flex ">
+        {openMenu ? (
+          <div
+            className="flex flex-col flex-1 py-4 px-10"
+            onClick={() => setOpenMenu((state) => !state)}
+          >
+            <Header />
+            <MenuOptions />
+          </div>
+        ) : (
+          <div
+            className="flex flex-1"
+            onClick={() => setOpenMenu((state) => !state)}
+          >
+            <SideBar />
+          </div>
+        )}
       </aside>
     </>
   );
