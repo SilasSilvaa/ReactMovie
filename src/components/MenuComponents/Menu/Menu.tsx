db0cd07ee183cb938d4235724841828179f2b89c
SilasSilvaa <silas-oli@hotamil.com>
2023-09-01 17:45:26 -0300

    refactor - ajuste no menu lateral

--- src/components/MenuComponents/Menu/Menu.tsx
+++ src/components/MenuComponents/Menu/Menu.tsx
@@ -2,29 +2,42 @@ import React, { useState } from 'react';
 import { MenuOptions } from '../MenuOptions/MenuOptions';
 import { Header } from '../Header/Header';
 import { SideBar } from '../SideBar/SideBar';
+import { CaretDoubleLeft, CaretDoubleRight } from '@phosphor-icons/react';
 
 export function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <>
-      <aside className="hidden flex-1 flex-col bg-black lg:flex ">
-        {openMenu ? (
+      <aside className=" hidden flex-1 flex-col bg-black lg:flex">
+        <div className="relative flex flex-1 rounded-lg p-2">
           <div
-            className="flex flex-col flex-1 py-4 px-10"
+            className="absolute w-8 h-8 right-[-2rem] top-5 z-50 bg-[#282726] rounded-lg 
+            cursor-pointer p-1"
             onClick={() => setOpenMenu((state) => !state)}
           >
-            <Header />
-            <MenuOptions />
+            {openMenu ? (
+              <CaretDoubleLeft className="w-full h-full text-white" />
+            ) : (
+              <CaretDoubleRight className="w-full h-full text-white" />
+            )}
           </div>
-        ) : (
-          <div
-            className="flex flex-1"
-            onClick={() => setOpenMenu((state) => !state)}
-          >
-            <SideBar />
-          </div>
-        )}
+          {openMenu ? (
+            <div
+              className="flex flex-1 flex-col border bg-[#282726] rounded-lg px-2 
+            transition-all ease-in"
+            >
+              <div className="p-2">
+                <Header />
+              </div>
+              <MenuOptions />
+            </div>
+          ) : (
+            <div className="flex flex-1  transition-all">
+              <SideBar />
+            </div>
+          )}
+        </div>
       </aside>
     </>
   );
