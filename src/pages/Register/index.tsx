904d8fdf23724d70104989ac2a75e9dada2ce240
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-01 22:45:41 -0300

    refactor - ajuste na estilização da pagina

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '../../components/Input/Input';
 import { Button } from '../../components/Button/Button';
-import { ArrowCircleLeft } from '@phosphor-icons/react';
+import { ArrowCircleLeft, ArrowLeft } from '@phosphor-icons/react';
 import { Link } from 'react-router-dom';
 
 export function Register() {
+  const [passwordInputValue, setPasswordInputValue] = useState('');
+
   return (
     <section className=" w-full h-full relative bg-black flex justify-center">
       <Link to={'/'}>
         <ArrowCircleLeft className="w-10 h-10 text-white absolute top-5 left-5 z-50" />
       </Link>
-      <div className="hidden relative items-center flex-col flex-1 lg:flex">
-        <img src="/src/assets/banner.jpg" alt="" className="w-full h-full" />
-        <h2 className="text-4xl absolute text-white font-bold text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          Create your free account
-        </h2>
+      {/* Imagem lateral */}
+      <div
+        className="hidden relative items-center flex-col w-3/5 bg-gradient-to-r 
+      from-red-500 via-red-800 lg:flex"
+      >
+        <div
+          className="flex items-center gap-2 flex-col absolute top-1/2 left-1/2 transform 
+        -translate-x-1/2 -translate-y-1/2 "
+        >
+          <h2 className="text-5xl text-white font-bold text-center">
+            Create new Accout
+          </h2>
+          <span className="text-xl text-white text-center">
+            Create your account for free.
+          </span>
+        </div>
       </div>
-      <div className="flex items-center justify-center flex-col gap-6 flex-1 p-4">
+      <div className="flex items-center justify-center flex-col gap-6 p-4 w-4/5 lg:w-2/5">
         <h2 className="text-2xl text-white font-bold p-6 text-center lg:hidden ">
-          Create your free account
+          Log-in in your account
         </h2>
-        <div className="w-full flex flex-col items-center p-6 gap-8 bg-black border-2 border-red-500 rounded-xl lg:max-w-2xl">
-          <form action="submit" className="flex flex-col w-full gap-4">
+        <div
+          className="w-full flex flex-col items-center p-6 gap-8 bg-black border-2 
+        border-red-500 rounded-xl lg:max-w-2xl"
+        >
+          <form action="submit" className="flex flex-col w-full gap-2">
             <div className="flex flex-col gap-2">
               <label htmlFor="" className="  text-white font-bold">
                 Name:
               </label>
-              <Input type="text" placeholder="write your name" />
+              <Input type="text" placeholder="write your e-mail" />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="" className="  text-white font-bold">
@@ -38,22 +54,35 @@ export function Register() {
               <label htmlFor="" className="  text-white font-bold">
                 Password:
               </label>
-              <Input type="text" placeholder="write your password" />
+              <Input
+                type="password"
+                value={passwordInputValue}
+                onChange={(e) => setPasswordInputValue(e.target.value)}
+                placeholder="write your password"
+              />
             </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="" className="  text-white font-bold">
-                Confirm passoword:
-              </label>
-              <Input type="password" placeholder="Confirm your password" />
-            </div>
+            {passwordInputValue && (
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="  text-white font-bold">
+                  Confirm your password:
+                </label>
+                <Input type="password" placeholder="write your password" />
+              </div>
+            )}
+
             <div className="w-full flex p-2 items-center justify-between gap-6">
-              <Link
-                to={'/account/login'}
-                className="text-white text-sm font-bold text-left cursor-pointer transition-colors ease-out hover:text-red-500"
-              >
-                Do you have account?
-              </Link>
-              <Button>Register</Button>
+              <div className="flex flex-col gap-2">
+                <span className="text-white text-sm font-bold text-left cursor-pointer transition-colors ease-out hover:text-red-500 ">
+                  Forgget you'r password?
+                </span>
+                <Link
+                  to={'/account/register'}
+                  className="text-white text-xs font-bold text-left cursor-pointer transition-colors ease-out hover:text-red-500 "
+                >
+                  Don't have account? Register
+                </Link>
+              </div>
+              <Button>Login</Button>
             </div>
           </form>
         </div>
