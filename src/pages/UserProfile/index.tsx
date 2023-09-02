4161b796a31deac4eac1600dc6b32fbfb940ad8d
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-02 11:27:31 -0300

    refactor - pagina de perfil com css refatorado

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,63 +1,93 @@
-import React from 'react';
-import { Button } from '../../components/Button/Button';
+import React, { useState } from 'react';
 import { Input } from '../../components/Input/Input';
+import { Button } from '../../components/Button/Button';
 
 export function UserProfile() {
+  const [isEdtingProfile, setIsEditingProfile] = useState(false);
+  const [inputPassword, setInputPassword] = useState('');
+
   return (
-    <div className="flex w-full flex-col justify-center items-center gap-6 p-6">
-      <p className="mediumTitle flex justify-center p-2">Profile</p>
-
-      <div className="w-full flex flex-row flex-wrap items-center justify-center max-w-4xl gap-6 border-2 border-red-500 rounded-lg p-2">
-        <div className="w-full flex items-center justify-center border-2 border-red-500 rounded-lg md:w-80 ">
-          <div className="flex items-center flex-col p-6 gap-6">
-            <img
-              src="/src/assets/film.jpg"
-              className="rounded-full w-24 h-24 object-cover"
-            />
-            <div className="flex flex-col items-center">
-              <span className="textCss text-white">Silas Silva</span>
-              <span className="text-white">Dev FullStack</span>
+    <section className="flex w-full flex-col justify-center items-center pt-20 p-2 lg:pt-0">
+      <div className="flex flex-col items-center p-6">
+        <h1 className="text-2xl text-white font-bold">Profile</h1>
+      </div>
+      <div className="w-full bg-[#282726] rounded-lg p-6 lg:max-w-lg">
+        <div className="flex flex-col items-center">
+          <div className="w-32 h-32 rounded-full bg-white lg:w-28 lg:h-28"></div>
+          <div className="w-full flex gap-4 p-4 items-center flex-wrap justify-center">
+            <Button className="lg:py-2">Edit Profile photo</Button>
+            <Button
+              className="lg:py-2"
+              onClick={() => setIsEditingProfile((state) => !state)}
+            >
+              Edit Profile
+            </Button>
+          </div>
+          <form action="" className="w-full">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="inputName" className="text-white font-bold">
+                Name:
+              </label>
+              <Input
+                id="inputName"
+                className=" bg-slate-200 text-black w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="inputEmail" className="text-white font-bold">
+                Email:
+              </label>
+              <Input
+                id="inputEmail"
+                className=" bg-slate-200 text-black w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="inputPassword" className="text-white font-bold">
+                Password:
+              </label>
+              <Input
+                id="inputPassword"
+                className=" bg-slate-200 text-black w-full"
+                value={inputPassword}
+                onChange={(e) => setInputPassword(e.target.value)}
+                type="password"
+              />
             </div>
 
-            <div className="flex flex-wrap gap-2 items-center justify-center">
-              <Button className="p-2 ">Edit photo</Button>
-              <Button className="p-2 ">Edit profile</Button>
+            <div>
+              {isEdtingProfile && (
+                <>
+                  {inputPassword && (
+                    <div className="flex flex-col gap-2">
+                      <label
+                        htmlFor="inputRepeatPassword"
+                        className="text-white font-bold"
+                      >
+                        Repeat your password:
+                      </label>
+                      <Input
+                        id="inputRepeatPassword"
+                        className=" bg-slate-200 text-black w-full"
+                        type="password"
+                      />
+                    </div>
+                  )}
+
+                  <div className="flex gap-4 pt-8 items-center justify-center">
+                    <Button className="bg-green-500 hover:bg-green-700">
+                      Confirm
+                    </Button>
+                    <Button onClick={() => setIsEditingProfile(false)}>
+                      Cancel
+                    </Button>
+                  </div>
+                </>
+              )}
             </div>
-          </div>
-        </div>
-        {/* <Input /> */}
-        <div className="flex flex-1 flex-col gap-4 p-6">
-          <div className="flex flex-1 flex-col gap-2">
-            <label htmlFor="" className="font-bold text-white">
-              Name
-            </label>
-            <Input />
-          </div>
-          <div className="flex flex-1 flex-col gap-2">
-            <label htmlFor="" className="font-bold text-white">
-              Full name
-            </label>
-            <Input />
-          </div>
-          <div className="flex flex-1 flex-col gap-2">
-            <label htmlFor="" className="font-bold text-white">
-              E-mail
-            </label>
-            <Input />
-          </div>
-          <div className="flex flex-1 flex-col gap-2">
-            <label htmlFor="" className="font-bold text-white">
-              password
-            </label>
-            <Input />
-          </div>
-          <div className="flex flex-1 justify-end p-2">
-            <span className="font-bold text-red-500 cursor-pointer">
-              View password
-            </span>
-          </div>
+          </form>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
