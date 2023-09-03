6af30a58662f160add679c19b225692f58b87d97
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-03 18:40:39 -0300

    refactor - adicionando skeleton componentes na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,34 +6,56 @@ import { Link } from 'react-router-dom';
 import { Card } from '../../components/Card/Card';
 import { Banner } from '../../components/Banner/Banner';
 import { Button } from '../../components/Button/Button';
+import { BannerSkeleton } from '../../components/SkeletonComponents/BannerSkeleton/BannerSkeleton';
+import { CardSkeleton } from '../../components/SkeletonComponents/CardSkeleton/CardSkeleton';
 
 export function Home() {
-  const { data: nowPlayingMovies } = useQuery('nowPlayingMovies', () =>
-    useGetAllMovies('movie/now_playing')
+  const { data: nowPlayingMovies, isLoading: isLoagingNowPlaying } = useQuery(
+    'nowPlayingMovies',
+    () => useGetAllMovies('movie/now_playing')
   );
 
-  const { data: upComingMovies } = useQuery('upComingMovies', () =>
-    useGetAllMovies('movie/upcoming')
+  const { data: upComingMovies, isLoading: isLoagingUpComing } = useQuery(
+    'upComingMovies',
+    () => useGetAllMovies('movie/upcoming')
   );
 
-  const { data: topRatedMovies } = useQuery('topRatedMovies', () =>
-    useGetAllMovies('movie/top_rated')
+  const { data: topRatedMovies, isLoading: isLoadingTopRated } = useQuery(
+    'topRatedMovies',
+    () => useGetAllMovies('movie/top_rated')
   );
 
   return (
     <>
       <section className="flex flex-col gap-4 pt-20 lg:pt-0">
-        <div className="p-2">
-          {upComingMovies && <Banner data={upComingMovies} isFilter />}
+        <div className="p-2 ">
+          {isLoagingUpComing ? (
+            <BannerSkeleton />
+          ) : (
+            upComingMovies && <Banner data={upComingMovies} isFilter />
+          )}
         </div>
-
         <div className="containerCard">
           <p className="mediumTitle">Upcoming</p>
           <div className="contentCard">
             <div className="carousel max-w-full gap-6 rounded-box">
-              {upComingMovies?.map((movie) => (
-                <Card detail={movie} key={movie.id} className="carousel-item" />
-              ))}
+              {isLoagingUpComing ? (
+                <>
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                </>
+              ) : (
+                upComingMovies?.map((movie) => (
+                  <Card
+                    detail={movie}
+                    key={movie.id}
+                    className="carousel-item"
+                  />
+                ))
+              )}
             </div>
             <div className="w-full flex justify-end p-2">
               <Link to={'/all/trending'}>
@@ -46,9 +68,23 @@ export function Home() {
           <p className="mediumTitle">Now playing</p>
           <div className="contentCard">
             <div className="carousel max-w-full gap-6 rounded-box">
-              {nowPlayingMovies?.map((movie) => (
-                <Card detail={movie} key={movie.id} className="carousel-item" />
-              ))}
+              {isLoagingNowPlaying ? (
+                <>
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                </>
+              ) : (
+                nowPlayingMovies?.map((movie) => (
+                  <Card
+                    detail={movie}
+                    key={movie.id}
+                    className="carousel-item"
+                  />
+                ))
+              )}
             </div>
             <div className="w-full flex justify-end p-2">
               <Link to={'/all/in_high'}>
@@ -62,9 +98,23 @@ export function Home() {
           <p className="mediumTitle">Top films</p>
           <div className="contentCard">
             <div className="carousel max-w-full gap-6 rounded-box">
-              {topRatedMovies?.map((movie) => (
-                <Card detail={movie} key={movie.id} className="carousel-item" />
-              ))}
+              {isLoadingTopRated ? (
+                <>
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                  <CardSkeleton />
+                </>
+              ) : (
+                topRatedMovies?.map((movie) => (
+                  <Card
+                    detail={movie}
+                    key={movie.id}
+                    className="carousel-item"
+                  />
+                ))
+              )}
             </div>
             <div className="w-full flex justify-end p-2">
               <Link to={'/all/top_films'}>
