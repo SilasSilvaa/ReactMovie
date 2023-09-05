c1406954dd6828de608a48bf52102ff2038f3c70
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-04 22:50:53 -0300

    feat - utilização de variaveis de ambiente

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const apikey = import.meta.env.VITE_API_KEY
+
 export const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjBhOWFmZGM2YjFjZDNiZWY1OGY1OWNmNmZkYmJjZiIsInN1YiI6IjY0ZGMwZGRhZjQ5NWVlMDI5MjUxMTVkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ukNdix_J_GcuBkNqiwzdFmsDMMoKo4Uff7O79Ls9Pwc'
+        Authorization: apikey
     },
     params: {
         language: 'pt-BR',
