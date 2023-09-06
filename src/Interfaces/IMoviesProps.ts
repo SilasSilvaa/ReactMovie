acd4d687a542108d4a3247a3ac81e54853e60803
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-05 23:32:43 -0300

    feat - criação um arquivo responsavel pelas interfaces

---
+++ src/Interfaces/IMoviesProps.ts
@@ -0,0 +1,46 @@
+export interface MoviesProps {
+    id: string;
+    title?: string;
+    name?: string;
+    poster_path?: string;
+    logo_path?: string;
+    backdrop_path?: string;
+    release_date?: string;
+    vote_average?: number;
+}
+
+export interface apiMovieProps {
+    dates: {
+        maximum: string;
+        minimum: string;
+    };
+    page: number;
+    results: MoviesProps[];
+    total_pages: string;
+    total_results: string;
+}
+
+export interface DetailMovieProps {
+    id: number;
+    title: string;
+    overview?: string;
+    backdrop_path?: string;
+    genres?: [
+        {
+            id: number;
+            name: string;
+        }
+    ];
+    homepage?: string;
+    production_companies?: [
+        {
+            id: number;
+            logo_path: string | null;
+            name: string;
+            origin_country: string;
+        }
+    ];
+    release_date?: string;
+    vote_average: number;
+}
+
