085cc9f9e8898e43553aff377b520fec533867c9
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-06 23:03:06 -0300

    feat - adicionado dados estaticos na pagina de favoritos

--- src/pages/Favourites/index.tsx
+++ src/pages/Favourites/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Filter } from '../../components/Filter/Filter';
-// import { Card } from '../../components/Card/Card';
+import { Card } from '../../components/Card/Card';
+import { useQuery } from 'react-query';
+import { useGetAllMovies } from '../../hooks/useGetAllMovies';
 
 export function Favourites() {
+  const { data } = useQuery('favourites', () =>
+    useGetAllMovies('movie/now_playing')
+  );
+
   return (
     <section className="flex flex-col pt-20 lg:pt-0">
       <div className="relative pb-10">
@@ -11,13 +17,11 @@ export function Favourites() {
 
       <div className="flex  flex-col gap-4 p-4 pt-20 items-center md:items-start">
         <h3 className="mediumTitle">Favourites</h3>
-        <div
-          className="flex flex-col flex-wrap gap-4 w-full items-center justify-center
-         md:flex-row md:items-start md:justify-start"
-        >
-          {/* <Card />
-          <Card />
-          <Card /> */}
+
+        <div className="flex flex-wrap gap-4 p-4 pt-10 justify-center">
+          {data?.map((movie) => (
+            <Card detail={movie} key={movie.id} />
+          ))}
         </div>
       </div>
     </section>
