b5d97c08bf56970f587f834950eebb551ba5f823
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-11 23:32:45 -0300

    feat - utilizando o hook de paginação teste

--- src/pages/AllMovies/index.tsx
+++ src/pages/AllMovies/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Filter } from '../../components/Filter/Filter';
 import { useGetAllMovies } from '../../hooks/useGetAllMovies';
+import { useTeste } from '../../hooks/useTeste';
 import { Card } from '../../components/Card/Card';
 
 export function AllMovies() {
+  const [page, setPage] = useState<number>(1);
+
   const { data } = useGetAllMovies('nowPlayingMovies', 'movie/now_playing');
+
+  const { data: testeData, refetch } = useTeste(
+    'nowPlay',
+    'movie/now_playing',
+    page
+  );
+
+  console.log(testeData);
+
+  useEffect(() => {
+    refetch();
+    console.log(testeData);
+    console.log(page);
+  }, [page]);
 
   return (
     <section className="flex flex-col pt-20 lg:pt-0">
@@ -37,7 +54,7 @@ export function AllMovies() {
         </div>
       </div>
       <div className="flex flex-wrap gap-4 p-4 pt-10 justify-center">
-        {data?.map((movie) => (
+        {testeData?.map((movie) => (
           <Card detail={movie} key={movie.id} className="w-56 h-56 text-xs" />
         ))}
       </div>
@@ -48,7 +65,10 @@ export function AllMovies() {
         <button className="w-8 h-8  bg-red-900 rounded-lg flex items-center justify-center ">
           <span className="text-white font-bold">1</span>
         </button>
-        <button className="w-8 h-8  bg-red-500 rounded-lg flex items-center justify-center">
+        <button
+          className="w-8 h-8  bg-red-500 rounded-lg flex items-center justify-center"
+          onClick={() => setPage((state) => state + 1)}
+        >
           <span className="text-white font-bold">2</span>
         </button>
         <button className="w-8 h-8  bg-red-500 rounded-lg flex items-center justify-center">
