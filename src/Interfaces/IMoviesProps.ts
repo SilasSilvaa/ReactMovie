0dfd8954b5bfc4af932664a7fc9bafff918f5de8
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-24 23:46:11 -0300

    refactor - Alteração na interface

--- src/Interfaces/IMoviesProps.ts
+++ src/Interfaces/IMoviesProps.ts
@@ -1,46 +1,45 @@
 export interface MoviesProps {
-    id: string;
-    title?: string;
-    name?: string;
-    poster_path?: string;
-    logo_path?: string;
-    backdrop_path?: string;
-    release_date?: string;
-    vote_average?: number;
+  id: string;
+  title?: string;
+  name?: string;
+  poster_path?: string;
+  logo_path?: string;
+  backdrop_path?: string;
+  release_date?: string;
+  vote_average?: number;
 }
 
 export interface ApiMovieProps {
-    dates: {
-        maximum: string;
-        minimum: string;
-    };
-    page: number;
-    results: MoviesProps[];
-    total_pages: string;
-    total_results: string;
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: MoviesProps[];
+  total_pages: string;
+  total_results: string;
+}
+
+export interface ProductCompaniesProps {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface GendersProps {
+  id: number;
+  name: string;
 }
 
 export interface DetailMovieProps {
-    id: number;
-    title: string;
-    overview?: string;
-    backdrop_path?: string;
-    genres?: [
-        {
-            id: number;
-            name: string;
-        }
-    ];
-    homepage?: string;
-    production_companies?: [
-        {
-            id: number;
-            logo_path: string | null;
-            name: string;
-            origin_country: string;
-        }
-    ];
-    release_date?: string;
-    vote_average: number;
+  id: number;
+  title: string;
+  overview?: string;
+  backdrop_path?: string;
+  genres?: GendersProps[];
+  homepage?: string;
+  production_companies?: ProductCompaniesProps[];
+  release_date?: string;
+  vote_average: number;
 }
-
