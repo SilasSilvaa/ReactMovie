ddf9ec9572ada0b343752477d714d7e9f45116e6
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-09-24 23:45:19 -0300

    feat - alteração nos parametros do filme e salvando os dados em session storage

--- src/hooks/useGetAllMovies.ts
+++ src/hooks/useGetAllMovies.ts
@@ -1,13 +1,18 @@
-import { useQuery } from 'react-query'
-import { api } from "../api/axios"
-import { ApiMovieProps } from "../Interfaces/IMoviesProps";
+import { useQuery } from 'react-query';
+import { api } from '../api/axios';
+import { ApiMovieProps } from '../Interfaces/IMoviesProps';
 
 export function useGetAllMovies(saveData: string, movieSearch: string) {
-    return useQuery(saveData, async () => {
-        const { data } = await api.get<ApiMovieProps>(movieSearch);
-
-        const dataMovies = data.results;
-
-        return dataMovies;
+  return useQuery(saveData, async () => {
+    const { data } = await api.get<ApiMovieProps>(movieSearch, {
+      params: {
+        language: 'en-US',
+      },
     });
-} 
+    const dataMovies = data.results;
+
+    sessionStorage.setItem(saveData, JSON.stringify(dataMovies));
+
+    return dataMovies;
+  });
+}
