573ca28e53d13d96474f3b7dd794e4575d1f15ca
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-10-04 01:18:04 -0300

    refactor - ajuste do prettier

--- src/hooks/useGetDetails.ts
+++ src/hooks/useGetDetails.ts
@@ -1,17 +1,15 @@
-import { useQuery } from 'react-query'
-import { api } from "../api/axios";
-import { DetailMovieProps } from "../Interfaces/IMoviesProps";
-
+import { useQuery } from 'react-query';
+import { api } from '../api/axios';
+import { DetailMovieProps } from '../Interfaces/IMoviesProps';
 
 export function useGetDetails(saveData: string, movieSearch: string) {
-
-    return useQuery(saveData, async () => {
-        const { data } = await api.get<DetailMovieProps>(movieSearch, {
-            params:{
-                language: 'en-US'
-            }
-        });
-
-        return data;
+  return useQuery(saveData, async () => {
+    const { data } = await api.get<DetailMovieProps>(movieSearch, {
+      params: {
+        language: 'en-US',
+      },
     });
+
+    return data;
+  });
 }
