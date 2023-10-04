2d87a1ade751f6252a7e3d450155aae8d8e9357a
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-10-04 01:16:11 -0300

    refactor - ajuste de css

--- src/components/MenuComponents/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/MenuComponents/HamburgerMenu/HamburgerMenu.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { MenuOptions } from '../MenuOptions/MenuOptions';
 import { X, List } from '@phosphor-icons/react';
-import { Header } from '../Header/Header';
 import { useLocation } from 'react-router-dom';
+import { Title } from '../Title/Title';
 
 export function HamburgerMenu() {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -18,9 +18,9 @@ export function HamburgerMenu() {
 
   return (
     <>
-      <div className="fixed w-full flex flex-col z-30 lg:hidden">
-        <div className="flex items-center p-6 justify-between bg-darkGray">
-          <Header />
+      <header className="fixed w-full flex flex-col z-30 lg:hidden">
+        <div className="flex items-center p-6 justify-between bg-black">
+          <Title />
           <div
             className="flex items-center justify-center w-10 h-10 rounded-full border-2 
             border-white bg-black cursor-pointer"
@@ -34,11 +34,11 @@ export function HamburgerMenu() {
           </div>
         </div>
         {toggleMenu && (
-          <div className="h-screen bg-darkGray">
+          <div className="h-screen bg-black">
             <MenuOptions />
           </div>
         )}
-      </div>
+      </header>
     </>
   );
 }
